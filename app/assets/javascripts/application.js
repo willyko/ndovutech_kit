7fe124f84d8a39c58e7bed1a2bf23e0cb6fb80e7
Willy Ko <willyko@y7mail.com>
2015-08-13 17:59:30 -0700

    added draggability of tools in kit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery-ui
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
---
+++ app/assets/javascripts/kits.js
@@ -0,0 +1,4 @@
+$(document).ready( function() {
+  $('.ui-widget-content').draggable();
+});
+
